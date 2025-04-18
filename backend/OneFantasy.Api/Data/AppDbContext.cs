ca73834a3cbfbc82354d7c62daa445e4b380e664
mreusm@uoc.edu <mreusm@uoc.edu>
2025-04-18 14:39:08 +0200

    Acabar de configurar back i front

---
+++ backend/OneFantasy.Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OneFantasy.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Aquí afegiràs els DbSet<T> quan tinguis entitats,
+        // per exemple:
+        // public DbSet<User> Users { get; set; }
+    }
+}
--- backend/OneFantasy.Api/Startup.cs
+++ backend/OneFantasy.Api/Startup.cs
@@ -1,51 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using OneFantasy.Api.Data;   // <- el teu namespace de l’AppDbContext
 
-namespace OneFantasy.Api
+public class Startup
 {
-    public class Startup
+    public IConfiguration Configuration { get; }
+    public Startup(IConfiguration configuration) => Configuration = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        // Afegeix aquest bloc:
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
-        public IConfiguration Configuration { get; }
+        // La resta de serveis ja existents:
+        services.AddControllers();
+        // ... qualsevol altra cosa que tinguis
+    }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // ... el teu pipeline (UseRouting, UseEndpoints, etc.)
     }
 }
